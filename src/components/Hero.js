22cafebbf1f4021855a269ec486d040887db342f
lukecheng1998 <lukecheng1998@gmail.com>
2020-06-26 11:55:56 -0700

    added some animations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,35 +1,34 @@
+import React, { useState } from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { render } from "react-dom";
+import { useSpring, animated } from "react-spring";
+function Hero(props) {
+    const style = useSpring({opacity: 1, from: {opacity: 0}})
+  return (
+    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+      <Container fluid={true}>
+        <Row className="justify-content-center py-5 ">
+          <Col md={8} sm={12}>
+            <animated.div style={style}>
+              {props.title && (
+                <h1 className="display-1 font-weight-bolder">{props.title}</h1>
+              )}
+              {props.subTitle && (
+                <h3 className="display-4 font-weight-light">
+                  {props.subTitle}
+                </h3>
+              )}
 
-import React, {useState} from 'react'
-import { Jumbotron } from 'react-bootstrap';
-import {Container, Row, Col} from 'react-bootstrap';
-import { render } from 'react-dom'
-import { useTrail, animated } from 'react-spring'
-function Hero (props){
-    const items = [props.title, props.subTitle, props.text]
-    const config = {mass: 5, tension: 2000, friction: 200}
-    const [toggle, set] = useState(true)
-    const trail = useTrail(items.length, {
-        config,
-        opacity:toggle ?1 : 0,
-        x: toggle ? 0 : 20,
-        height: toggle ? 80 : 0,
-        from: { opacity: 0, x: 20, height: 0},
-    })
-    return (
-        <Jumbotron className="bg-transparent jumbotron-fluid p-0">
-            <Container fluid={true}>
-                <Row className="justify-content-center py-5">
-                    <Col md={8} sm={12}>
-                        <animated.div>
-                            {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
-                            {props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3>}
-                            {props.text && <h3 className="lead font-weight-light">{props.text}</h3>}
-                        </animated.div>
-                    </Col>
-                </Row>
-            </Container>
-        </Jumbotron>
-    );
+              {props.text && (
+                <h3 className="lead font-weight-light">{props.text}</h3>
+              )}
+            </animated.div>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react'
-import Hero from '../components/Hero'
-import Content from '../components/content'
-import AboutCarousel from '../components/AboutCarousel'
-import Portrait from '../assets/images/portrait.jpg';
-function AboutPage (props) {
-    return(
-       <div>
-           <Hero title={props.title} />
-           <Content>
-               <img src={Portrait} className="portrait-format center"/>
-               <p>Hello I'm Luke Cheng current a student studying at Purdue University.</p>
-               <p>I enjoy doing backend and front end development and I'm looking for more!</p>
-            <AboutCarousel />
-           </Content>
-       </div>
-    )
+import React, { Component } from "react";
+import Hero from "../components/Hero";
+import Content from "../components/content";
+import AboutCarousel from "../components/AboutCarousel";
+import Portrait from "../assets/images/portrait.jpg";
+function AboutPage(props) {
+  return (
+    <div>
+      <Hero title={props.title} />
+      <Content>
+        <img src={Portrait} className="portrait-format center" />
+        <p>
+          Hello I'm Luke Cheng current a student studying at Purdue University.
+        </p>
+        <p>
+          I enjoy doing backend and front end development and I'm looking for
+          more!
+        </p>
+        <AboutCarousel />
+      </Content>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
