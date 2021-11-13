5905d01408e1ce107f798c2871de7a88f4878d60
Luke Cheng <lukecheng1998@gmail.com>
2021-11-12 21:27:03 -0800

    updated ProjectsPage

--- src/components/ProjectsPageCarousel.js
+++ src/components/ProjectsPageCarousel.js
@@ -2,8 +2,18 @@ import React, { Component } from "react";
 import { Container, Row } from "react-bootstrap";
 class ProjectsPageCarousel extends Component {
     constructor(props){
-        this.defaultState = {
-
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: "Talk Purdue",
+                    body: "Talk Purdue a social media application to collaborate with other people. See more of it here!" 
+                },
+                {
+                    id: 1,
+                    title: "Honestly"
+                }
+            ]
         }
     }
 }
